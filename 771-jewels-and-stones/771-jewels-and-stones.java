57e1e76e895653cee7fbe261aea07a393121b658
Js111l <95163990+Js111l@users.noreply.github.com>
2022-06-15 20:03:12 +0200

    Time: 1 ms (96.93%), Space: 42.8 MB (19.82%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -0,0 +1,20 @@
+class Solution
+{
+
+    public int numJewelsInStones(String jewels, String stones)
+    {
+        int counter = 0;
+        for(int i = 0;i<jewels.length();i++){
+            for (int j = 0;j<stones.length();j++){
+                if(jewels.charAt(i)==stones.charAt(j)){
+                    counter++;
+                }
+
+            }
+        }
+return counter;
+
+
+
+    }
+}
