d56decbd0e02725f3a510bb165d8ed0cdf2ee21a
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-06-21 20:30:15 +0200

    Time: 53 ms (54.85%), Space: 58.8 MB (76.11%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,24 +1,23 @@
 class Solution {
-    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        Arrays.sort(products);
-        
-        List<List<String>> res = new ArrayList<>();
-        for (int i = 0; i < searchWord.length(); i++) {
-            String word = searchWord.substring(0,i+1);
-            List<String> list = new ArrayList<>();
-            for (String product : products) {
-                if (product.length() < word.length())
-                    continue;
-                if (list.size() >= 3)
-                    break;
-                String sub = product.substring(0,i+1);
-                if (word.equals(sub))
-                    list.add(product);
-            }
+   static public List<List<String>> suggestedProducts(String[] products, String searchWord) {
 
-            res.add(list);
+        Arrays.sort(products);
+        List<List<String>> list=new ArrayList<>();
+        for (int i = 0; i < searchWord.length(); i++) {
+            List<String> listL = new ArrayList<>();
+            String prefix=searchWord.substring(0,1+i);
+            for (String product:products
+                 ) {
+                if(product.startsWith(prefix)){
+                    listL.add(product);
+                }
+                if(listL.size()>=3){
+                    break;
+                }
+            }
+            list.add(listL);
         }
-        
-        return res;
+        return list;
+
     }
 }
