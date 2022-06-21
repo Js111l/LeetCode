702d3f0375c8f352251d79da2196a64083f7b4b2
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-06-21 20:17:30 +0200

    Time: 197 ms (20.85%), Space: 63.2 MB (52.16%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,19 +1,15 @@
 class Solution {
-    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-
-
+   static public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+       
         Arrays.sort(products);
 
-
         List<List<String>> list=new ArrayList<>();
-
         List<String> list1 = new ArrayList<>();
 
-        StringBuilder stringBuilder = new StringBuilder();
-
         for (int i = 0; i < searchWord.length(); i++) {
-            stringBuilder.append(searchWord.charAt(i));
-            list1=Arrays.stream(products).filter(x->x.startsWith(stringBuilder.toString()))
+
+            String prefix=searchWord.substring(0,1+i);
+            list1=Arrays.stream(products).filter(x->x.startsWith(prefix))
                     .limit(3).collect(Collectors.toList());
             list.add(list1);
         }
