0975b3cee6733c137d2d5185214f81781583c5c2
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-06-30 19:01:07 +0200

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.java
@@ -0,0 +1,65 @@
+// { Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    ArrayList<Integer> res = new ArrayList<>();
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<ArrayList<Integer>> adj1=adj;
+
+        boolean[] visited = new boolean[V];
+        DFSHELPER(0,visited,adj1);
+        return res;
+        
+
+
+    }
+    void DFSHELPER(int n, boolean[] visited, ArrayList<ArrayList<Integer>> adj1) {
+
+            if (!visited[n]) {
+                visited[n]=true;
+                res.add(n);
+            }else {
+                return;
+            }
+            for(int i=0;i<adj1.get(n).size();i++){
+                DFSHELPER(adj1.get(n).get(i),visited,adj1);
+            }
+
+    
+    }
+
+}
+
+
