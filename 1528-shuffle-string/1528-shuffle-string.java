647cbf00aa7da044f692cd6d4354606d8bc8291e
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-07-21 19:08:45 +0200

    Time: 11 ms (13.45%), Space: 46.5 MB (13.17%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -1,16 +1,19 @@
-class Solution{
-    public String restoreString(String s, int[] indices)
-    {
-        char [] arr = new char[s.length()];
+class Solution {
+    public String restoreString(String s, int[] indices) {
 
-        for(int i = 0; i<indices.length;i++)
-        {
-            arr[indices[i]]=s.charAt(i);
-
-        }
-
-String naz = new String(arr);
-
-        return   naz;
+      Map<Integer,Character> map=new TreeMap<>();
+      
+      for(int i=0;i< indices.length;i++){
+          map.put(indices[i],s.charAt(i));
+      }
+        
+      StringBuilder res=new StringBuilder();
+      
+      for(int i=0;i< indices.length;i++){
+          res.append(map.get(i));
+      }
+      
+      return    res.toString();
+      
     }
 }
