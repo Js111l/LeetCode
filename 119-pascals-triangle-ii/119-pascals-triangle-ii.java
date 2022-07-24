368bb4f2ce7239fae67e274218f97909a5e623b1
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-07-24 21:39:24 +0200

    Time: 11 ms (5.11%), Space: 41.8 MB (53.30%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -0,0 +1,39 @@
+
+class Solution {
+    public List<Integer> getRow(int k)
+    {
+        List<Integer> ans = new ArrayList<>();
+
+        if(k <= 0)
+        {
+            ans.add(1);
+            return ans;
+        }
+
+        return recurse(k,ans,1);
+    }
+
+    List<Integer> recurse(int k, List<Integer> list, int n)
+    {
+        if(n<=k)
+        {
+            List<Integer> tmp = new ArrayList<>();
+            tmp.add(1);     
+            //1
+                    System.out.println(list.size());
+
+
+            for(int i=1; i<list.size(); i++)
+            {
+                tmp.add(list.get(i)+list.get(i-1));
+
+            }
+            tmp.add(1);
+
+            return recurse(k,tmp,n+1);
+
+        }
+        return list;
+    }
+
+}
