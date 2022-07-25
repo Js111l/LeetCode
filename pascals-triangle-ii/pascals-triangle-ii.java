9a92ca13e32f458fa6eb2f6ea7bd4a165561b791
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-07-25 16:58:27 +0200

    Time: 19 ms, Memory: 42.5 MB - LeetHub

--- pascals-triangle-ii/pascals-triangle-ii.java
+++ pascals-triangle-ii/pascals-triangle-ii.java
@@ -9,8 +9,10 @@ class Solution {
             ans.add(1);
             return ans;
         }
+        ans.add(1);
+        ans.add(1);
 
-        return recurse(k,ans,1);
+        return recurse(k,ans,2);
     }
 
     List<Integer> recurse(int k, List<Integer> list, int n)
@@ -20,11 +22,12 @@ class Solution {
             List<Integer> tmp = new ArrayList<>();
             tmp.add(1);     
             //1
-                    System.out.println(list.size());
 
 
             for(int i=1; i<list.size(); i++)
             {
+                System.out.println(list.get(i));
+
                 tmp.add(list.get(i)+list.get(i-1));
 
             }
