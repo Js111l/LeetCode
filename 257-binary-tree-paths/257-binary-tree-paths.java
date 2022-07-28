0d75afb1f80e6115a06ab9cd8cb22829abb38c0f
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-07-28 20:57:31 +0200

    Time: 6 ms (69.60%), Space: 42.7 MB (85.35%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -1,3 +1,4 @@
+
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
 
@@ -9,21 +10,20 @@ class Solution {
     }
 
     private void helper(TreeNode root, List<String> strings, StringBuilder stringBuilder) {
-    if (root==null){
-        return;
+   if(root==null){
+       return;
+   }
+                int l=stringBuilder.length();
+
+    if(root.right!=null||root.left!=null){
+        stringBuilder.append(root.val+"->");
     }
-    int l=stringBuilder.length();
-          if(root.left!=null||root.right!=null)
-        stringBuilder.append(root.val);
-    
     if(root.left==null&&root.right==null){
         stringBuilder.append(root.val);
         strings.add(stringBuilder.toString());
     }
-    stringBuilder.append("->");
-    helper(root.left,strings,stringBuilder);
     helper(root.right,strings,stringBuilder);
+    helper(root.left,strings,stringBuilder);
     stringBuilder.setLength(l);
-
     }
 }
