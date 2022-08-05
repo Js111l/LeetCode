b6a24eb49aaba4b2eefd53b047c85a70f18e5d37
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-08-05 11:19:04 +0200

    Time: 1 ms (65.35%), Space: 42.6 MB (10.57%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,14 +1,25 @@
-
 class Solution {
+    int index=-1;
     public int strStr(String haystack, String needle) {
-      return  haystack.indexOf(needle);
+
+     if(needle.isEmpty()){
+         return 0;
+     }
+     helper(haystack,needle,0);
+     return index;
+    }
+
+    private void helper(String haystack, String needle, int i) {
+
+        if(haystack.startsWith(needle)){
+            index=i;
+            return;
+        }
+        if(haystack.length()!=1){
+        helper(haystack.substring(1),needle,i+1);
+        }
+        
     }
 }
-
-
-
-
-
-
 
 
