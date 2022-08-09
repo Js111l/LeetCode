da6018956951cd2bb9b547b476c62de1681c1b48
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-08-09 14:24:14 +0200

    Time: 3 ms (67.32%), Space: 68.2 MB (40.22%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    int indx;
+
+    public boolean canJump(int[] nums) {
+        int v = 0;
+        for (int i = 0; i <= v; i++) {
+            v=Math.max(v,nums[i]+i);
+            if(v>=nums.length-1){
+            return true;
+        }
+        }
+        
+        return false;
+    }
+
+}
+
+
+
