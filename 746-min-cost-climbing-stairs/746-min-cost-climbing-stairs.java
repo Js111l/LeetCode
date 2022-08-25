56a55bb9c7dd792ba96d39988776d377319b3878
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-08-25 20:01:20 +0200

    Time: 1 ms (89.83%), Space: 43.4 MB (70.81%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        if (cost.length == 2) {
+            return Math.min(cost[0], cost[1]);
+        }
+        for (int i = cost.length - 3; i >= 0; i--) {
+                cost[i]+=Math.min(cost[i + 1], cost[i + 2]);
+            }
+        return Math.min(cost[0],cost[1]);
+    }
+}
