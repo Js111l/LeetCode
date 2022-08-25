4079a13289d2c0c901ce09a7398cdb34d1d19a55
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-08-25 15:11:24 +0200

    Time: 299 ms (30.47%), Space: 42.2 MB (96.26%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -4,22 +4,21 @@ class Solution {
         String res = "";
         for (int i = 0; i < s.length(); i++) {
             for (int j = s.length() - 1; j >= 0; j--) {
-                String temp = s.substring(i, j + 1);
                 if (j - i + 1 <= res.length()) {
                     break;
                 }
-                if (checkIfPalindrome(temp)) {
-                    res = temp;
+                if (checkIfPalindrome(s,i,j)) {
+                    res = s.substring(i, j + 1);
                 }
             }
         }
         return res;
     }
-    private boolean checkIfPalindrome(String temp) {
-      int low=0;
-      int high=temp.length()-1;
-        while(low<high){
-            if(temp.charAt(low++)!=temp.charAt(high--)){
+    private boolean checkIfPalindrome(String temp,int low,int high) {
+      int first=low;
+      int last=high;
+        while(first<last){
+            if(temp.charAt(first++)!=temp.charAt(last--)){
                 return false;
             }
         }
