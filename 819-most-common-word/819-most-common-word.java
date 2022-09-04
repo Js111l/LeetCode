de531779d6bfa6dd004fed6fb0409ec7270a18e8
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-09-04 18:52:57 +0200

    Time: 386 ms (5.02%), Space: 46.8 MB (5.24%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.stream.Collectors;
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        paragraph=paragraph.replaceAll("[^a-zA-Z0-9]"," ");
+        paragraph=paragraph.replaceAll("\\s+",".");
+        String [] array =paragraph.split("[.]");
+        array= Arrays.stream(array).map(x->x.trim().toLowerCase(Locale.ROOT)).collect(Collectors.toList()).toArray(String[]::new);
+        Map<String,Integer> map=new HashMap<>();
+        Set<String> set= Arrays.stream(array).collect(Collectors.toSet());
+        for (String s :set) {
+            map.put(s,0);
+        }
+        Set<String> bannedWords= Arrays.stream(banned).map(x->x.trim().toLowerCase(Locale.ROOT)).collect(Collectors.toSet());
+        for (int i = 0; i < array.length; i++) {
+            if(!bannedWords.contains(array[i])&&map.keySet().contains(array[i])){
+                map.put(array[i],map.get(array[i])+1);
+            }
+        }
+        Integer max=map.values().stream().toList().stream().max(Comparator.naturalOrder()).get();
+       Optional<Map.Entry<String,Integer>> optional=map.entrySet().stream().
+               max((Map.Entry<String,Integer>e1,Map.Entry<String,Integer>e2)->e1.getValue().compareTo(e2.getValue()));
+        return optional.get().getKey();
+    }
+}
