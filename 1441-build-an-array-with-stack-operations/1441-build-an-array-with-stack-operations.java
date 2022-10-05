dd2ec36a367a9fbc3a7c5fea51f1894344be0a99
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-10-05 14:06:25 +0200

    Time: 14 ms (12.28%), Space: 44.5 MB (5.15%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,13 +1,12 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        List<Integer> list = Arrays.stream(target).mapToObj(x -> Integer.valueOf(x)).collect(Collectors.toList());
         List<String> list1 = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
-            if(list.equals(temp)){
+            if(temp.equals(Arrays.stream(target).mapToObj(x->Integer.valueOf(x)).collect(Collectors.toList()))){
                 break;
             }
-            if (list.contains(Integer.valueOf(i))) {
+            if (Arrays.binarySearch(target,i)>=0){
                 list1.add("Push");
                 temp.add(i);
             }else{
