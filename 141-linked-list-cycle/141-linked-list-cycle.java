5a594ffc52cda038105d8027132e943777239999
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-10-05 17:07:44 +0200

    Time: 19 ms (5.49%), Space: 47.4 MB (10.98%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        
+        
+
+        ListNode node = head;
+        HashMap<ListNode,Boolean> visited=new HashMap<>();
+        while (node != null) {   //traverse all nodes
+            if(visited.get(node)==null){ //if node hasn't been visited yet, add it to map.
+                visited.put(node,true);
+                node=node.next;
+            }else         //if node has been visited already -> there is a cycle. Return that node.
+                return true;
+        }
+        return false;
+    }
+
+
+    }
