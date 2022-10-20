e217947617fdec4f9bd980af5691b11534ede028
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-10-20 15:05:59 +0200

    Time: 665 ms (5.28%), Space: 43 MB (85.10%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,22 +1,23 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         List<String> stringList = List.of(words);
+        Comparator<String>comparator=new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int freqO1 = Collections.frequency(List.of(words), o1);
+                int fregO2 = Collections.frequency(List.of(words), o2);
+
+                if (freqO1 != fregO2) {
+                    return fregO2 - freqO1;
+                }
+                if (freqO1 == freqO1) {
+                    return o1.compareTo(o2);
+                }
+                return -1;
+            }
+        };
         return Arrays.stream(words).
                 sorted(Comparator.comparing(x -> Collections.frequency(stringList, x))).
-                distinct().sorted(new Comparator<String>() {
-                    @Override
-                    public int compare(String o1, String o2) {
-                        int freqO1 = Collections.frequency(List.of(words), o1);
-                        int fregO2 = Collections.frequency(List.of(words), o2);
-
-                        if (freqO1 != fregO2) {
-                            return fregO2 - freqO1;
-                        }
-                        if (freqO1 == freqO1) {
-                            return o1.compareTo(o2);
-                        }
-                        return -1;
-                    }
-                }).limit(k).collect(Collectors.toList());
+                distinct().sorted(comparator).limit(k).collect(Collectors.toList());
     }
 }
