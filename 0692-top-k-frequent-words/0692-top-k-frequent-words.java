bffb5fc66f951db8d48376e762bab30224b2f110
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-10-20 17:22:37 +0200

    Time: 136 ms (5.28%), Space: 72.9 MB (5.32%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,11 +1,18 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String,Integer> hashMap=new HashMap<>();
+        for (String word:words) {
+            if(hashMap.containsKey(word)){
+                hashMap.put(word,hashMap.get(word)+1);
+            }else
+                hashMap.put(word,1);
+        }
         Comparator<String> comparator = new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
                 List<String> list=List.of(words);
-                int freqO1 = Collections.frequency(list, o1);
-                int fregO2 = Collections.frequency(list, o2);
+                int freqO1 = hashMap.get(o1);
+                int fregO2 = hashMap.get(o2);
 
                 if (freqO1 != fregO2) {
                     return fregO2 - freqO1;
@@ -17,11 +24,11 @@ class Solution {
             }
         };
 
-        Set<String> set=new TreeSet<>(comparator);
+        Set<String> priorityQueue=new TreeSet<>(comparator);
         for (String w : words
         ) {
-            set.add(w);
+            priorityQueue.add(w);
         }
-        return set.stream().limit(k).toList();
+        return priorityQueue.stream().limit(k).toList();
     }
 }
