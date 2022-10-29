9a8725056d4d24e7049a1d1af38c836318924fc3
Jakub Świercz <95163990+Js111l@users.noreply.github.com>
2022-10-29 09:52:54 +0200

    Time: 12 ms (85.23%), Space: 41.2 MB (95.01%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.java
@@ -0,0 +1,37 @@
+
+class Foo {
+
+    public Foo() {
+
+    }
+
+    private volatile AtomicInteger integer = new AtomicInteger(1);
+
+    public synchronized void first(Runnable printFirst) throws InterruptedException {
+        while (integer.get()!=1){
+            wait();
+        }
+        integer.incrementAndGet();
+        printFirst.run();
+        notifyAll();
+    }
+
+    public synchronized void second(Runnable printSecond) throws InterruptedException {
+        while (integer.get() != 2) {
+            wait();
+        }
+        integer.incrementAndGet();
+        printSecond.run();
+        notifyAll();
+    }
+
+
+    public synchronized void third(Runnable printThird) throws InterruptedException {
+        while (integer.get() != 3) {
+            wait();
+        }
+        printThird.run();
+        notifyAll();
+    }
+}
+
